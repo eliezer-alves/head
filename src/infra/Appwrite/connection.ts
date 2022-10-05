ddb9996f36be4e000ef95761b0b310afa9788b71
Eliezer Alves <eliezer.cardoso@venturus.org.br>
2022-10-05 08:54:28 -0300

    feat: (remote-add-account) domain anda data layer

--- src/infra/Appwrite/connection.ts
+++ src/infra/Appwrite/connection.ts
@@ -1,7 +1,9 @@
 import { Client, Account, ID } from 'appwrite'
 
 export const client = new Client()
-  .setEndpoint('http://http://192.168.65.108//v1') // Your API Endpoint
+  .setEndpoint(
+    'appwrite client --selfSigned true --endpoint http://localhost/v1'
+  ) // Your API Endpoint
   .setProject('633acaa9071bb839463c') // Your project ID
 
 export const account = new Account(client)
