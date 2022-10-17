095783fb4225141c4697665b4e028c3f9ec46126
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2022-10-17 11:34:18 -0300

    chore: (infra/Firebase) Firebase SDK connection configuration

---
+++ src/infra/Firebase/config/connection.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SEND_ID,
+  appId: import.meta.env.VITE_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage()
+
+export { auth, db, storage }
