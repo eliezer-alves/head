0ccc05600b45ae9389e379c425390e40f4ab099c
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2022-10-19 08:35:25 -0300

    chore: jest configuration to test react components

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     '<rootDir>/node_modules/',
     '<rootDir>/tests/e2e/cypress',
   ],
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
   },
