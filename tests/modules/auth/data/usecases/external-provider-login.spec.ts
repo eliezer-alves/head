4592a77002b3edb5eed6064947092b9b36c5b982
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2022-10-26 02:48:16 -0300

    test: should instantiate the ExternalProviderLogin use case

--- tests/modules/auth/data/usecases/external-provider-login.spec.ts
+++ tests/modules/auth/data/usecases/external-provider-login.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { ExternalProviderLogin } from '@auth/data/usecases'
 
 describe('Auth - External Provider Login', () => {
-  it('Deve instanciar o caso de uso Login', () => {
+  it('Should instantiate the ExternalProviderLogin use case', () => {
     const sut = new ExternalProviderLogin()
     expect(sut).not.toBeNull()
   })
