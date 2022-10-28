e24e8ae111564ce1274bccfdda7ee0f6ad11524c
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2022-10-27 23:11:43 -0300

    feat: [customer] handling return from httpClient

--- src/modules/customer/data/usecases/remote-add-customer.ts
+++ src/modules/customer/data/usecases/remote-add-customer.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@/common/http'
+import { AccessDeniedError, UnexpectedError } from '@/common/errors'
+import { HttpClient, HttpStatus } from '@/common/http'
 import { AddCustomer } from '@customer/domain/usecases'
 
 export class RemoteAddCustomer {
@@ -7,11 +8,20 @@ export class RemoteAddCustomer {
     private readonly httpClient: HttpClient<AddCustomer.Model>,
   ) {}
   async exec(params: AddCustomer.Params): Promise<any> {
-    return this.httpClient.request({
+    const httpResponse = await this.httpClient.request({
       method: 'post',
       url: this.url,
       body: params,
     })
+
+    switch (httpResponse.status) {
+      case HttpStatus.ok:
+        return httpResponse.body
+      case HttpStatus.forbidden:
+        throw new AccessDeniedError()
+      default:
+        throw new UnexpectedError()
+    }
   }
 }
 
