23f29f6b88b3862d8fc11f3209d320921d3d4011
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2022-10-28 18:39:05 -0300

    test: (infra) Stripe - Should correctly instantiate Stripe's customers.create resource

---
+++ tests/infra/Stripe/stripe-resource.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { stripe } from '@/infra/Stripe/config/connection'
+import { StripeResource } from '@/infra/Stripe/resources'
+
+describe('Stripe Resourcer', () => {
+  it("should correctly instantiate Stripe's customers.create resource", async () => {
+    const sut = StripeResource('/customer', 'post')
+    expect(sut).equals(stripe.customers.create)
+  })
+})
